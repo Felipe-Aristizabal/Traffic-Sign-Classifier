a7f1ee0fb90c452bb17dc392e011ad89cfd92c23
Hector-f-Romero <hector.f.romero28@gmail.com>
2023-11-11 22:00:41 -0500

    Sockets implementation
    Integration of Flask_socketIO to send information of predicted image in real time to client. Has been configured cors to allow all aplications.

--- app/main.py
+++ app/main.py
@@ -1,26 +1,63 @@
 import os
+import time
 
 from flask import Flask, render_template, Response
+from flask_socketio import SocketIO
 from dotenv import load_dotenv
+from flask_cors import CORS
 import cv2
+
+from utils.predict_images import preprocessing_images, load_predict_model
 
 # take environment variables from .env.
 load_dotenv()
 
-# Indicate t
 app = Flask(__name__)
-
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 # Capture the default camera capture device
 cap = cv2.VideoCapture(0)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+
+# Load the model that we want to use
+model = load_predict_model()
+
+
+def predict_image(image):
+    start_predict = time.time()
+
+    predicts = (model.predict(image))
+
+    if (max(predicts[0]) < 0.5):
+        print(" NO PREDIJO NADA")
+    else:
+        object_predited = None
+        print(predicts.argmax())
+        if (predicts.argmax() == 0):
+            object_predited = "Speed"
+        elif (predicts.argmax() == 1):
+            object_predited = "Stop"
+        elif (predicts.argmax() == 2):
+            object_predited = "Traffic light"
+        socketio.emit(
+            "prediction", {"value": object_predited})
+    end_predict = time.time()
+    print(f"Time: {end_predict-start_predict} seconds")
 
 
 def capture_camera():
     while True:
         ret, frame = cap.read()
+
         # Flip the camera
         frame = cv2.flip(frame, 1)
 
         # TODO: build the preprocessing here
+        image = preprocessing_images(frame)
+
+        # PREDICT IMAGE
+        predict_image(image)
+
         (flag, encodedImage) = cv2.imencode(".jpg", frame)
         if not flag:
             continue
@@ -33,11 +70,6 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/cosa")
-def index1():
-    return Response("Hola")
-
-
 @app.route("/video_feed")
 def video_feed():
     return Response(capture_camera(),
@@ -45,4 +77,5 @@ def video_feed():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=os.getenv("PORT"), host=os.getenv("HOST"))
+    socketio.run(app=app, debug=True, port=os.getenv(
+        "PORT"), host=os.getenv("HOST"))
---
+++ app/utils/predict_images.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
+
+
+def preprocessing_images(image):
+
+    resized_img = cv2.resize(image, (150, 150))
+    # Normalize the pixel values since 0 to 255
+    normalized_img = resized_img / 255.0
+    # Convert image to tensor
+    tensor_img = np.reshape(normalized_img, (1, 150, 150, 3))
+    return tensor_img
+
+
+def load_predict_model():
+    # Obtener el directorio actual (donde se encuentra test-model.py)
+    current_directory = Path.cwd()
+
+    # Construir la ruta hacia first-model.h5
+    model_path = current_directory / 'app' / 'models' / 'first-model.h5'
+
+    model_test = load_model(model_path)
+    return model_test
+#  while cap.isOpened():
+#         _, frame = cap.read()
+#         key = cv2.waitKey(1)
+#         # Reducimos el tamaño de la imagen para disminuir el costo de computo
+#         frame = cv2.resize(
+#             frame, (int(frame.shape[1]/2), int(frame.shape[0]/2)))
+
+#         frame = cv2.flip(frame, 1)
+
+#         cv2.imshow("Result", frame)
+
+#         print("Predicción: ")
+#         predict_melo(frame)
+#         print("--------------------------------------")
